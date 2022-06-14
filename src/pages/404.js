488d190ab81286d35419492eed37fb6553d209dc
ayan4m1 <andrew@bulletlogic.com>
2022-06-14 19:43:19 -0400

    amend page title for not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,7 +9,7 @@ export default function NotFoundPage({ data }) {
 
   return (
     <Layout title={siteTitle}>
-      <SEO title="404: Not Found" />
+      <SEO title="Not Found" />
       <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </Layout>

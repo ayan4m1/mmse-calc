dd9e8b40d39098da15163da3b4bc2f8f6a19455f
ayan4m1 <andrew@bulletlogic.com>
2022-06-16 21:28:18 -0400

    start implementing calculator

--- src/pages/calculator.js
+++ src/pages/calculator.js
@@ -1,34 +1,57 @@
-import { Fragment, useCallback, useState } from 'react';
-import { Form, Row, Col, Button } from 'react-bootstrap';
+import { useCallback, useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
 import { useFormik } from 'formik';
 
 import Layout from '~components/Layout';
+import { scoringData } from '~utils/scoring';
 
 export default function CalculatorPage() {
-  const [results, setResults] = useState({});
+  const [tScore, setTScore] = useState(null);
   const { errors, touched, handleChange, handleSubmit, values } = useFormik({
     initialValues: {
-      age: 0
+      age: 0,
+      educationLevel: 0,
+      score: 0
     },
     validate: useCallback((vals) => {
-      const { age } = vals;
+      const { age, educationLevel, score } = vals;
       const result = {};
 
-      if (age < 1) {
+      if (age < 0) {
         result.age = 'Patient is too young.';
-      } else if (age > 30) {
-        result.age = 'Patient is too old.';
+      }
+
+      if (educationLevel < 0) {
+        result.educationLevel =
+          'Patient cannot have less than zero years education.';
+      }
+
+      if (score < 0) {
+        result.score = 'Score is too low.';
+      } else if (score > 30) {
+        result.score = 'Score is too high.';
       }
 
       return result;
     }, []),
     onSubmit: useCallback(
       (vals) => {
-        const { age } = vals;
+        const { age, educationLevel, score } = vals;
 
-        setResults({ age });
+        for (const { ages, educationLevels } of scoringData) {
+          if (age >= ages.min && age <= ages.max) {
+            for (const { years, results } of educationLevels) {
+              if (educationLevel >= years.min && educationLevel <= years.max) {
+                setTScore(results[30 - score]);
+                return;
+              }
+            }
+          }
+        }
+
+        setTScore(-1);
       },
-      [setResults]
+      [setTScore]
     )
   });
 
@@ -49,16 +72,40 @@ export default function CalculatorPage() {
             </Form.Control.Feedback>
           )}
         </Form.Group>
+        <Form.Group>
+          <Form.Label>Patient Education Level (Years)</Form.Label>
+          <Form.Control
+            name="educationLevel"
+            value={values.educationLevel}
+            onChange={handleChange}
+            isInvalid={Boolean(errors.educationLevel)}
+          />
+          {errors.educationLevel && touched.educationLevel && (
+            <Form.Control.Feedback type="invalid">
+              {errors.educationLevel}
+            </Form.Control.Feedback>
+          )}
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Raw Score</Form.Label>
+          <Form.Control
+            name="score"
+            value={values.score}
+            onChange={handleChange}
+            isInvalid={Boolean(errors.score)}
+          />
+          {errors.score && touched.age && (
+            <Form.Control.Feedback type="invalid">
+              {errors.score}
+            </Form.Control.Feedback>
+          )}
+        </Form.Group>
         <Form.Group className="my-2">
           <Button type="submit">Submit</Button>
         </Form.Group>
       </Form>
-      {results.age && (
-        <Fragment>
-          <Row>
-            <Col>Age: {results.age}</Col>
-          </Row>
-        </Fragment>
+      {Boolean(tScore) && (
+        <h1 className="m-4">T-score is {tScore === -1 ? 'Unknown' : tScore}</h1>
       )}
     </Layout>
   );
